416c7f0469735e0eb969823fc7c9f8eb88df7b0b
rossijonas <rossijonas@pm.me>
2022-09-25 18:13:25 -0300

    terraform provision gcp artifact registry and compute engine

--- base-test-api-infra/main.tf
+++ base-test-api-infra/main.tf
@@ -1,1 +1,61 @@
+# Create Artifact Registry Repository
+resource "google_artifact_registry_repository" "base-test-api" {
+  location      = var.region
+  repository_id = "base-test-api-compute-engine"
+  description   = "Host base-test-api docker image"
+  format        = "DOCKER"
+}
 
+# Create a single Compute Engine instance
+resource "google_compute_instance" "default" {
+  name         = "flask-vm"
+  machine_type = "f1-micro"
+  zone         = var.zone
+  tags         = ["ssh"]
+
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python-pip rsync; pip install flask"
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
+
+# Allow SSH connection
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = "default"
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+
+# Open port 8080 on the VM
+resource "google_compute_firewall" "flask" {
+  name    = "flask-app-firewall"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
